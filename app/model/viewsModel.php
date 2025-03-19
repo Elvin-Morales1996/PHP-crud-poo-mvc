167056316f8aaf0d3dfcd7ab1c4b518e0e56f52a
elvin morales <morales.elvin.16@gmail.com>
2025-03-19 15:20:57 -0600

    modelo que controla la vista

--- app/model/viewsModel.php
+++ app/model/viewsModel.php
@@ -9,9 +9,34 @@
 namespace app\model;
 class viewsModel{
 
+//una funcion que estara protegida.
+/*
+una funcion que maneja las vistas. estara protegida y que solo esta clase puede usar 
+y las que hereden de ella
 
 
-}
+*/
+protected function obtenerVistasModel($vista){
+    //creando un arreglo con las vistas que se van a mostrar.
+    $listaBlanca = ["dashboard"];
+
+    if (in_array($vista, $listaBlanca)) {
+        if (is_file("./app/views/content/".$vista."-view.php")) {
+            $contenido = "./app/views/content/".$vista."-view.php";
+        }else{
+            $contenido="404";
+        }//else del if mayor
+    }//if
+    elseif($vista=="login" || $vista=="index"){
+            $contenido = "login";
+
+    } //elseif
+    else{
+        $contenido = "404";
+    }//else
+    return $contenido;
+}//es la funcion
+}//es de la clase
 
 ?>
 
